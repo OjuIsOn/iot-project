05cb9666fb1104b523f3db3be7cc735fdbd462a9
OjuIsOn <ojasvisingh542.os@gmail.com>
2024-10-01 02:26:01 +0530

    adf

--- routes/cycle.js
+++ routes/cycle.js
@@ -80,6 +80,8 @@ router.post('/location', (req, res) => {
   if (latitude && longitude) {
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
     
+    let latitudeInt = parseInt(latitude, 10);   // Converts to base-10 integer
+    let longitudeInt = parseInt(longitude, 10);
 
     res.status(200).json({
         message: 'Location data received successfully!',
