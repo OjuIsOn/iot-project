b17ba1c3833bee620e361536526f8ce5fd23008a
OjuIsOn <ojasvisingh542.os@gmail.com>
2024-10-31 18:19:11 +0530

    backtoBasic

--- routes/user.js
+++ routes/user.js
@@ -31,7 +31,7 @@ Router.post("/register",async (req,res)=>{
             password:password,
             cycleNum:cycleNum
         }).then(User => {
-            res.redirect("/api/user/login");
+            res.redirect("/api/cycle");
         })
         .catch(error => {
             
@@ -67,16 +67,6 @@ Router.post("/login",async (req,res)=>{
     res.redirect("/api/menu");
 
 })
-
-Router.get('/configurations',async (req,res) => {
-    try {
-    const users = await User.find(); // Fetch all users
-    res.render('configurations', { users }); // Render the EJS template and pass the users data
-} catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching users');
-}
-});
 
 Router.get('/history', restrictToLoggedinUser, async (req, res) => {
     try {
